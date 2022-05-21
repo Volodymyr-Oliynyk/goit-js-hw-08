c1acd31f2cb12e6ba3e85e452b5b6a3843bf3a10
Volodymyr-Oliynyk <93007917+Volodymyr-Oliynyk@users.noreply.github.com>
2022-05-21 17:35:51 +0300

    part 1 task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,36 @@
 import throttle from "lodash.throttle";
 
+const feedbackForm = document.querySelector('.feedback-form');
+const FEEDBACK_KEY = 'feedback-form-state';
+let formData = {};
+
+const getValue = localStorage.getItem(FEEDBACK_KEY);
+const dataValueSave = JSON.parse(getValue);
+
+const onInputData = e => {
+    formData[e.target.name] = e.target.value;
+    localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
+  };
+
+  const onFormSubmit = e => {
+    e.preventDefault();
+ 
+    // if (feedbackForm.elements.email.value == '' || feedbackForm.elements.message.value == '') {
+    //   alert('Заполни пустые поля');
+    //   return;
+    // }
+    localStorage.removeItem(FEEDBACK_KEY);
+    e.currentTarget.reset();
+    console.log(dataValueSave);
+  };
+  
+  function saveInputData() {
+    if (dataValueSave) {
+      Object.keys(dataValueSave).forEach(item => (feedbackForm[item].value = dataValueSave[item]));
+    }
+  }
+  
+  saveInputData();
+  
+  feedbackForm.addEventListener('input', throttle(onInputData, 500));
+  feedbackForm.addEventListener('submit', onFormSubmit);
